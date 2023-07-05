a29e771af660c6443232b981f2ed8afee853b96e
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-07-05 03:10:39 -0400

    funcion para verificar archivo
    Verificar si es un archivo o directorio y a las vez filtrar archivos md

--- index.js
+++ index.js
@@ -5,23 +5,69 @@ const filePath = process.argv[2];
 
 const isAbsolute = (route) => {
   return path.isAbsolute(route)
-}
+};
 
 const relativeToAbsolute = (route) => {
   return path.resolve(route)
-}
+};
 
 const exist = (route) => {
   return new Promise((resolve, reject) => {
     fs.stat(route, (err, stats) => {
-      if (err) {
-        reject(false);
-      } else {
-        resolve(true);
-      }
+      err ? reject(false) : resolve(true);
     });
   });
 };
+
+// const contentDirectory = (route) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readdir(route, (err, files) => {
+//       err ? reject(err) : resolve(files)
+//     });
+//   });
+// };
+
+// const filterContentDirectory = (route) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readdir(route, (err, files) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         const filteredFiles = files.filter((file) => path.extname(file) === '.md');
+//         resolve(filteredFiles);
+//       }
+//     });
+//   });
+// }
+
+const checkFileType = (route) => {
+  return new Promise((resolve, reject) => {
+    fs.stat(route, (err, stats) => {
+      if (err) {
+        reject(err);
+      } else {
+        // si es un archivo
+        if (stats.isFile()) {
+          const fileExtension = path.extname(route);
+          // filtrar si es md
+          fileExtension === '.md' ? resolve(true) : reject(false);
+          // si es un directorio
+        } else if (stats.isDirectory()) {
+          fs.readdir(route, (err, files) => {
+            if (err) {
+              reject(err);
+            } else {
+              // filtrar si es md
+              const filteredFiles = files.filter((file) => path.extname(file) === '.md');
+              resolve(filteredFiles);
+            }
+          });
+        }
+      }
+    })
+  })
+};
+
 
 
 module.exports = {
@@ -29,4 +75,5 @@ module.exports = {
   filePath,
   relativeToAbsolute,
   exist,
+  checkFileType,
 }
